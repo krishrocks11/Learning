4b354a312e9ba00a989b37c9ecb8b3f80d726fa5
Shiva Sreenivasan <krishrocks11@gmail.com>
2021-10-05 19:43:11 +0800

    cosmetic changes

--- NN Assignment 1/nn_rbf_gaussian.py
+++ NN Assignment 1/nn_rbf_gaussian.py
@@ -18,27 +18,27 @@ label_train = loadmat('label_train.mat')
 db_data_train = data_train["data_train"]
 db_label_train = label_train["label_train"]
 db_data_test = data_test["data_test"]
-num_centers = 200
-db_weight = np.zeros([num_centers,1])
-centers = np.zeros([num_centers,33])
-index_seed = random.sample(range(0,329), num_centers)
+num_centres = 200
+db_weight = np.zeros([num_centres,1])
+centres = np.zeros([num_centres,33])
+index_seed = random.sample(range(0,329), num_centres)
 #print(index_seed)
-for i in range(num_centers):
-    centers[i,:] = db_data_train[index_seed[i]]
+for i in range(num_centres):
+    centres[i,:] = db_data_train[index_seed[i]]
     
-#Calculate Sigma based on Centers. sigma = dmax/sqrt(2m). m is number of centers. dmax is max distance between centers
+#Calculate Sigma based on centres. sigma = dmax/sqrt(2m). m is number of centres. dmax is max distance between centres
 d_max = 0
-for i in range(num_centers):
-    for j in range(num_centers):
-        if distance.euclidean(centers[i], centers[j]) > d_max:
-            d_max = distance.euclidean(centers[i], centers[j])
+for i in range(num_centres):
+    for j in range(num_centres):
+        if distance.euclidean(centres[i], centres[j]) > d_max:
+            d_max = distance.euclidean(centres[i], centres[j])
 
 #print(db_data_train)
 #print(db_data_test)
 #print(db_label_train)
 #print(db_weight)
 
-#For Only 2 Centers. Each Center for each Class
+#For Only 2 centres. Each Center for each Class
 db_class1 = np.zeros([1,33])
 db_class2 = np.zeros([1,33])
 num_class_1 = 0
@@ -55,8 +55,8 @@ for i in range(len(db_data_train)):
 db_avg1 = db_class1/num_class_1
 db_avg2 = db_class2/num_class_2
 
-#Calculate Sigma based on centers
-ligma = d_max/math.sqrt(2*num_centers)
+#Calculate Sigma based on centres
+ligma = d_max/math.sqrt(2*num_centres)
 
 def fi(x,c,sigma):
     db_phi = np.zeros([len(x),len(c)])
@@ -67,14 +67,14 @@ def fi(x,c,sigma):
             
     return db_phi
 
-db_fi = fi(db_data_train, centers, ligma)
+db_fi = fi(db_data_train, centres, ligma)
 db_weight = np.matmul(np.linalg.pinv(db_fi),db_label_train)
 
 #print(db_weight)
 
-def acc(x,lablez, centers, sigma, w):
+def acc(x,lablez, centres, sigma, w):
     num_corr = 0
-    predicts = np.matmul(fi(x, centers, sigma), w) 
+    predicts = np.matmul(fi(x, centres, sigma), w) 
     threshold = np.average(np.unique(lablez))
     for i in range(len(predicts)):
       if predicts[i] > threshold:
@@ -87,9 +87,9 @@ def acc(x,lablez, centers, sigma, w):
     acc = num_corr/len(x)
     return acc
 
-print(acc(db_data_train,db_label_train, centers, ligma, db_weight))
+print(acc(db_data_train,db_label_train, centres, ligma, db_weight))
 
-prediction = np.matmul(fi(db_data_test, centers, ligma), db_weight)
+prediction = np.matmul(fi(db_data_test, centres, ligma), db_weight)
 for i in range(len(prediction)):
       if prediction[i] > 0:
         prediction[i] = 1
